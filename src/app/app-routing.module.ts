9325e067da949c771a8d388e91fd36644b8eda17
adrian <adriangtoff@gmail.com>
2023-06-21 20:48:14 -0600

    Adrian Morales Lopez

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './componentes/login/login.component';
+import { PaginaComponent } from './componentes/pagina/pagina.component';
+import { RegistroComponent } from './componentes/registro/registro.component';
+import { ArticuloComponent } from './componentes/articulo/articulo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'login',component:LoginComponent},
+  {path:'',component:LoginComponent},
+  {path:'pagina',component:PaginaComponent},
+  {path:'registro',component:RegistroComponent},
+  {path:'articulo',component:ArticuloComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { RegistroComponent } from './componentes/registro/registro.component';
+import { PaginaComponent } from './componentes/pagina/pagina.component';
+import { ArticuloComponent } from './componentes/articulo/articulo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistroComponent,
+    PaginaComponent,
+    ArticuloComponent
   ],
   imports: [
     BrowserModule,
